22192712f9b14c1e71d0e4064b5d313a5f1378c3
Navid Jalilian <n.jalilian@aranumadc.local>
2024-03-21 01:01:15 +0330

    add run and bootstrap files

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div id="cart-text">Cart: ${faker.random.number()}</div>`;
-
-document.querySelector("#dev-cart").innerHTML = cartText;
+import('./bootstrap')
---
+++ run.bat
@@ -0,0 +1,1 @@
+concurrently "cd cart && npm run start" "cd products && npm run start" "cd container && npm run start"
